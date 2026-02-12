2056829c955780a11e03c44dacd65fa10fb7e2b4
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-02-13 00:57:42 +0100

    Add additional creality constants

--- tag/creality/constants.py
+++ tag/creality/constants.py
@@ -10,7 +10,30 @@ class CrealityAdditonalData:
         self.drying_time_hours = drying_time_hours
 
 CREALITY_FILAMENT_CODE_TO_DATA = {
-    "01001": CrealityAdditonalData("PLA", ["Hyper"], 190.0, 230.0, 60.0, 55.0, 8.0),
+    "01001": CrealityAdditonalData("PLA", ["Hyper"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "02001": CrealityAdditonalData("PLA-CF", ["Hyper"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "06002": CrealityAdditonalData("PETG", ["Hyper"], 220.0, 270.0, 70.0, 60.0, 8.0),
+    "03001": CrealityAdditonalData("ABS", ["Hyper"], 240.0, 280.0, 80.0, 80.0, 8.0),
+    "09002": CrealityAdditonalData("PLA", ["Ender", "Fast"], 190.0, 240.0, 50.0, 50.0, 6.0),
+    "04001": CrealityAdditonalData("PLA", ["CR"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "05001": CrealityAdditonalData("PLA", ["CR", "Silk"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "06001": CrealityAdditonalData("PETG", ["CR"], 220.0, 270.0, 70.0, 60.0, 8.0),
+    "07001": CrealityAdditonalData("ABS", ["CR"], 240.0, 280.0, 100.0, 80.0, 8.0),
+    "08001": CrealityAdditonalData("PLA", ["Ender"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "09001": CrealityAdditonalData("PLA", ["EN", "PLA+"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "10001": CrealityAdditonalData("TPU", ["HP"], 190.0, 240.0, 40.0, 65.0, 8.0),
+    "11001": CrealityAdditonalData("PA", ["CR", "Nylon"], 250.0, 270.0, 50.0, 80.0, 8.0),
+    "13001": CrealityAdditonalData("PLA", ["CR", "Carbon"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "14001": CrealityAdditonalData("PLA", ["CR", "Matte"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "15001": CrealityAdditonalData("PLA", ["CR", "Fluo"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "16001": CrealityAdditonalData("TPU", ["CR"], 210.0, 240.0, 40.0, 65.0, 8.0),
+    "17001": CrealityAdditonalData("PLA", ["CR", "Wood"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "18001": CrealityAdditonalData("PLA", ["HP", "Ultra"], 190.0, 240.0, 50.0, 50.0, 8.0),
+    "19001": CrealityAdditonalData("ASA", ["HP"], 240.0, 280.0, 90.0, 80.0, 8.0),
+    "12003": CrealityAdditonalData("PA-CF", ["Hyper", "PAHT"], 280.0, 320.0, 90.0, 80.0, 10.0),
+    "12002": CrealityAdditonalData("PA-CF", ["Hyper", "PPA"], 280.0, 320.0, 100.0, 100.0, 8.0),
+    "07002": CrealityAdditonalData("PC", ["Hyper"], 250.0, 270.0, 110.0, 80.0, 8.0),
+    "01601": CrealityAdditonalData("PLA", ["Soleyin", "Ultra"], 190.0, 240.0, 50.0, 50.0, 8.0),
 }
 
 CREALITY_SALT_HASH = "e544d94feb16159bbd7bc227df1e283eca1f38f2bb2015dfcc6161b74473b5c2"
